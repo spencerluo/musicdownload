ddabc79ba0e02216622f3a26d8f3dc37d290cde5
spencerluo <spencerluo@s3graphics.com>
2018-08-28 17:14:50 +0800

    update

--- kuwo.py
+++ kuwo.py
@@ -3,6 +3,7 @@ import re
 import os
 import requests
 import bs4
+import sys
 
 
 class Kuwo:
@@ -16,29 +17,40 @@ class Kuwo:
         res.raise_for_status()
 
         if '很抱歉，没有找到相关的结果' in res.text:
-            return
+            raise ValueError('have no such song [{}]'.format(song_name))
 
-        soup = bs4.BeautifulSoup(res.text)
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
         lists = soup.find_all('li', class_="clearfix")
         for li in lists:
             song_info = li.find('a')
             song_href = song_info.attrs.get('href')
+            song_title = song_info.attrs.get('title')
             song_id = re.search('\d+', song_href).group()
+
+            album = li.find_all('a')[1]
+            album_name = album.attrs.get('title')
 
             singer = li.find_all('a')[2]
             singer_title = singer.attrs.get('title')
 
-            self.songs.append(Song(song_name, song_id, singer_title))
+            self.songs.append(Song(song_title, song_id, album_name, singer_title))
         return self.songs
+
+    def show_songs(self):
+        song_list = [map(len, song.get_list()) for song in self.songs]
+        size = list(map(max, zip(*song_list)))
+        formatter = '   '.join(['{%s:<%s}' % item for item in enumerate(size)])
+        print('\n'.join([str(index+1)+'. '+formatter.format(*song.get_list()) for index, song in enumerate(self.songs)]))
 
 
 class Song:
     player_url = 'http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_{}'
 
-    def __init__(self, song_name, song_id, singer):
+    def __init__(self, song_name, song_id, album_name, singer):
         self.song_name = song_name
         self.id = song_id
         self.singer = singer
+        self.album_name = album_name
 
     def download(self, save_path='./result'):
         res = requests.get(self.player_url.format(self.id))
@@ -55,9 +67,9 @@ class Song:
             os.mkdir(save_path)
 
         i = 1
-        file_path = os.path.join(save_path, self.song_name)
+        file_path = os.path.join(save_path, self.song_name + '.mp3')
         while os.path.exists(file_path):
-            file_path = os.path.join(save_path, self.song_name + '({})'.format(i))
+            file_path = os.path.join(save_path, self.song_name + '({})'.format(i) + '.mp3')
             i += 1
 
         with open(file_path, 'wb') as f:
@@ -65,7 +77,27 @@ class Song:
 
         print('download [{}] success'.format(self.song_name))
 
+    def __repr__(self):
+        return '{} {} {}'.format(self.song_name, self.album_name, self.singer)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def get_list(self):
+        return [self.song_name, self.album_name, self.singer]
+
 
 if __name__ == '__main__':
-    ku = Kuwo()
-    ku.search_song('七里香')[0].download()
+
+    while True:
+        ku = Kuwo()
+        song = input('what song do you want to download: \n')
+        try:
+            ku.search_song(song)
+        except ValueError:
+            print('搜索不到歌曲')
+            continue
+        ku.show_songs()
+        num = input('which one do you want to download: (1~10)\n')
+
+        ku.songs[int(num)-1].download()
